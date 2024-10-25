780d7bb2b1f2f03cdec9b59a546fe102da39abc1
piotrselak <piotr.selak.it@gmail.com>
2024-10-26 01:50:57 +0200

    Working on executing generic tasks

--- include/thread_pool/executor.h
+++ include/thread_pool/executor.h
@@ -10,22 +10,33 @@
 // class IThreadPool {
 // };
 
-inline void worker_thread(const std::shared_ptr<ThreadSafeQueue<Task> > &queue) {
-}
 
 class Executor {
 public:
-    explicit Executor(const int thread_num = 4) : queue_(std::make_shared<ThreadSafeQueue<Task> >()) {
+    explicit Executor(const int thread_num = 4) : queue_(std::make_shared<ThreadSafeQueue<ITask> >()) {
         for (int i = 0; i < thread_num; i++)
             workers.emplace_back(worker_thread, queue_);
     };
 
     ~Executor() = default; // TODO: graceful shutdown? idk if here
 
-    template<typename T>
-    std::shared_ptr<Task<T> > compute(std::function<T> f);
+    template<typename R, typename Func, typename... Args>
+    std::shared_ptr<Task<R> > compute(Func &&f, Args &&... args) {
+        std::function<R()> task_function = std::bind(f, args);
+        auto task = std::make_shared<Task<R> >(Task<R>(task_function));
+        queue_->enqueue(task);
+        return task;
+    }
 
 private:
     std::vector<std::thread> workers;
-    std::shared_ptr<ThreadSafeQueue<Task> > queue_;
+    std::shared_ptr<ThreadSafeQueue<ITask> > queue_{}; // TODO queue of shared_ptr?
+
+    [[noreturn]] static void worker_thread(const std::shared_ptr<ThreadSafeQueue<ITask> > &queue) {
+        while (true) {
+            // TODO change while
+            if (queue->is_empty()) continue;
+            ITask task = queue->dequeue();
+        }
+    }
 };
--- include/thread_pool/task.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-template<typename T>
-class Task<T> {
-};
