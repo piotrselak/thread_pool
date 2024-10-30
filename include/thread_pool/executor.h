db52914099f03ff998ddfe6e903d7cb1ea688e63
piotrselak <piotr.selak.it@gmail.com>
2024-10-30 17:58:44 +0100

    Removed tasks and added future handling

--- include/thread_pool/executor.h
+++ include/thread_pool/executor.h
@@ -1,14 +1,12 @@
 #pragma once
 
+#include <any>
 #include <memory>
 #include <thread_safe_queue.h>
 #include <thread>
 #include <functional>
+#include <future>
 
-#include "task.h"
-
-// class IThreadPool {
-// };
 
 // TODO runtime management of adjusting threads/detecting ideal number
 class Executor {
@@ -29,18 +27,20 @@ public:
         }
     }
 
-    // TODO rework to std::futures
     template<typename R, typename Func, typename... Args>
-    std::shared_ptr<Task<R> > compute(Func &&f, Args &&... args) {
+    std::future<R> compute(Func &&f, Args &&... args) {
         std::function<R()> task_function = std::bind(f, args...);
-        auto task = std::make_shared<Task<R> >(task_function);
-        queue_->enqueue(task);
-        return task;
+
+        std::packaged_task<R()> pt(task_function);
+        std::future<R> result = pt.get_future();
+        queue_->enqueue(pt);
+
+        return result;
     }
 
 private:
     std::vector<std::thread> workers;
-    std::shared_ptr<ThreadSafeQueue<std::shared_ptr<ITask> > > queue_;
+    std::shared_ptr<ThreadSafeQueue<std::packaged_task<std::any()> > > queue_;
     std::atomic<bool> shutdown_;
 
     // TODO for now its very simple
@@ -49,9 +49,8 @@ private:
     void worker_thread() {
         while (!shutdown_) {
             if (queue_->is_empty()) continue;
-            auto task = queue_->dequeue();
-            task->execute();
-            task->set_completed();
+            auto pt = queue_->dequeue();
+            pt();
         }
     }
 };
